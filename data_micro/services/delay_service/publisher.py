ed843dab14ccf5ec063b1620e4f520199052a7ce
Anton <ANTON-1996@yandex.ru>
2025-04-26 17:11:05 +0300

    откорректировал паблишеры микросервиса на обратную доставку данных, нужно уточнение по данным из бд, в каком формате получаются (dict или list)

--- data_micro/services/delay_service/publisher.py
+++ data_micro/services/delay_service/publisher.py
@@ -1,66 +1,71 @@
 import json
 
 from nats.js.client import JetStreamContext
+# from tenacity import retry # библиотека для повторных отправок сообщений
 
 # брокеры сообщений - паблишеры
 
-# получение списка всех дк
-async def get_dk_list_publisher(
+# отправка списка всех дк
+async def push_dk_list_publisher(
         js: JetStreamContext,
         chat_id: int,
+        data: dict,
         subject: str
 ) -> None:
 
     payload = json.dumps({
-        'chat_id': chat_id
+        'chat_id': chat_id,
+        'data': dict
     }).encode()
 
     await js.publish(subject=subject, payload=payload)
 
 
-# получение списка выданных промокодов
-async def get_promocode_list_publisher(
+# отправка списка выданных промокодов
+async def push_promocode_list_publisher(
         js: JetStreamContext,
         chat_id: int,
+        data: dict,
         subject: str
 ) -> None:
 
     payload = json.dumps({
-        'chat_id': chat_id
+        'chat_id': chat_id,
+        'data': data
     }).encode()
 
     await js.publish(subject=subject, payload=payload)
 
 
-# уточнение информации по карте покупателя
-async def get_dk_info_publisher(
+# отправка информации по карте покупателя
+async def push_dk_info_publisher(
         js: JetStreamContext,
         chat_id: int,
         dk: int,
         dk_owner: str,
+        info: str,
         subject: str
 ):
     payload = json.dumps({
         'chat_id': chat_id,
         'dk': dk,
-        'dk_owner': dk_owner
+        'dk_owner': dk_owner,
+        'info': info
     })
 
     await js.publish(subject=subject, payload=payload)
 
 
-# получение промокода покупателем
-async def get_promocode_publisher(
+# отправка промокода покупателем
+async def push_promocode_publisher(
         js: JetStreamContext,
         chat_id: int,
-        dk: int,
-        dk_owner: str,
+        promocode: int,
         subject: str
 ):
     payload = json.dumps({
         'chat_id': chat_id,
-        'dk': dk,
-        'dk_owner': dk_owner
+        'promocode': promocode
     })
 
     await js.publish(subject=subject, payload=payload)
