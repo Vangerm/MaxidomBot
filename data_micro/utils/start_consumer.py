2d68a6a191f555bbba2eb7b6e66be69ff35c32ab
ANTON <ANTON-1996@yandex.ru>
2025-04-27 22:16:39 +0300

    добавил запуск консьюмеров у микросервиса

--- data_micro/utils/start_consumer.py
+++ data_micro/utils/start_consumer.py
@@ -1,7 +1,12 @@
 import logging
 
 from aiogram import Bot
-from services.delay_service.consumer import VkPostConsumer
+from services.delay_service.consumer import (
+    GetDkListConsumer,
+    GetPromocodeListConsumer,
+    GetDkInfoConsumer,
+    GetPromocodeConsumer
+)
 
 from nats.aio.client import Client
 from nats.js.client import JetStreamContext
@@ -10,19 +15,39 @@ from nats.js.client import JetStreamContext
 logger = logging.getLogger(__name__)
 
 
-async def start_poll_dk_info(
+async def start_poll_dk_list(
         nc: Client,
         js: JetStreamContext,
-        bot: Bot,
         subject_consumer: str,
+        subject_publisher: str,
         stream: str,
         durable_name: str
         ) -> None:
-    consumer = VkPostConsumer(
+    consumer = GetDkListConsumer(
         nc=nc,
         js=js,
-        bot=bot,
         subject_consumer=subject_consumer,
+        subject_publisher=subject_publisher,
+        stream=stream,
+        durable_name=durable_name
+    )
+    logger.info('Start poll dk info consumer')
+    await consumer.start()
+
+
+async def start_poll_dk_info(
+        nc: Client,
+        js: JetStreamContext,
+        subject_consumer: str,
+        subject_publisher: str,
+        stream: str,
+        durable_name: str
+        ) -> None:
+    consumer = GetDkInfoConsumer(
+        nc=nc,
+        js=js,
+        subject_consumer=subject_consumer,
+        subject_publisher=subject_publisher,
         stream=stream,
         durable_name=durable_name
     )
@@ -33,16 +58,16 @@ async def start_poll_dk_info(
 async def start_poll_promocode(
         nc: Client,
         js: JetStreamContext,
-        bot: Bot,
         subject_consumer: str,
+        subject_publisher: str,
         stream: str,
         durable_name: str
         ) -> None:
-    consumer = VkPostConsumer(
+    consumer = GetPromocodeConsumer(
         nc=nc,
         js=js,
-        bot=bot,
         subject_consumer=subject_consumer,
+        subject_publisher=subject_publisher,
         stream=stream,
         durable_name=durable_name
     )
@@ -53,16 +78,16 @@ async def start_poll_promocode(
 async def start_poll_promocode_list(
         nc: Client,
         js: JetStreamContext,
-        bot: Bot,
         subject_consumer: str,
+        subject_publisher: str,
         stream: str,
         durable_name: str
         ) -> None:
-    consumer = VkPostConsumer(
+    consumer = GetPromocodeListConsumer(
         nc=nc,
         js=js,
-        bot=bot,
         subject_consumer=subject_consumer,
+        subject_publisher=subject_publisher,
         stream=stream,
         durable_name=durable_name
     )
